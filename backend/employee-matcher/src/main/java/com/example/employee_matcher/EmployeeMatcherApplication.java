2a4df5ccb5ce416317579cabe8b18cb1a55f11ba
Tisho <tihomirpetrov79@gmail.com>
2025-05-29 18:23:35 +0300

    Initial commit with Angular and Spring Boot projects

---
+++ backend/employee-matcher/src/main/java/com/example/employee_matcher/EmployeeMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMatcherApplication.class, args);
+	}
+
+}
